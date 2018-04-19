0a1aefddd2d5289263d835315e1358a64a305d04
Mathieu AGAR <magar@mirakl.com>
2018-04-19 19:12:33 +0200

    Adding spring to deps

---
+++ src/main/java/info/magat/appcxmapper/ContextMapper.java
@@ -0,0 +1,10 @@
+package info.magat.appcxmapper;
+
+import org.springframework.context.ApplicationContext;
+
+public class ContextMapper {
+
+    public void map(ApplicationContext ctx){
+        //This is where the magic happens
+    }
+}
